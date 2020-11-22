98e9e718f11ab5777b6c0614af7e55f0d34fae54
Cam9Cam9Cam9 <74627066+Cam9Cam9Cam9@users.noreply.github.com>
2020-11-21 19:43:20 -0500

    Add files via upload
    Here is my current version. The only error is below the code naming portion at the top. I'm not sure how to fix this

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/combined_teleop_pogram.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/combined_teleop_pogram.java
@@ -7,11 +7,10 @@ import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 
-@TeleOp(name="teleop_program")
-public class teleop_program extends LinearOpMode {
+@TeleOp(name="combined teleop program")
+    public class combined_teleop_program extends LinearOpMode {
 
     // Declare OpMode members.
-    DcMotor intake;
     DcMotor FL;
     DcMotor FR;
     DcMotor BL;
@@ -31,7 +30,7 @@ public class teleop_program extends LinearOpMode {
     boolean LeftBumper;
     boolean dpad_right;
     boolean dpad_left;
-    boolean button2A;
+
     double current_power;
     Servo loadingservo;
     boolean GOOD = true;
@@ -49,8 +48,6 @@ public class teleop_program extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
 
-       intake = hardwareMap.dcMotor.get("intake");
-        
         Flywheel1 = hardwareMap.dcMotor.get("Left");
         Flywheel2 = hardwareMap.dcMotor.get("Right");
         Flywheel2.setDirection(DcMotor.Direction.REVERSE);
@@ -87,17 +84,19 @@ public class teleop_program extends LinearOpMode {
             dpad_down = gamepad1.dpad_down;
             dpad_left = gamepad1.dpad_left;
             dpad_right = gamepad1.dpad_right;
-            
-            button2A = gamepad2;
 
-            if (buttonX = true) {
+            if (buttonX == true) {
                 Motor.setPower(0.80);
             }
-            if (buttonX = false) {
+            if (buttonX == false) {
                 Motor.setPower(0.00);
             }
 
 
+            {
+
+
+            }
 
     /*void xRailLower() {
         GOOD=1;
@@ -112,131 +111,113 @@ public class teleop_program extends LinearOpMode {
         GOOD=0;
     }*/
 
-            void stopIsPushed () {
-                FL.setPower(0.00);
-                FR.setPower(0.00);
-                BL.setPower(0.00);
-                BR.setPower(0.00);
-                telemetry.addData("Path", "Complete");
-                telemetry.update();
-                idle();
-            }
+                if (buttonB) {
+                    telemetry.addData("Stopping All Flywheel Motors.", " ");
+                    telemetry.update();
+                    current_power = Range.clip(current_power, -1, 1);
+                    Flywheel1.setPower(0.00);
+                    Flywheel2.setPower(0.00);
+                }
 
 
-            if (buttonB) {
-                telemetry.addData("Stopping All Flywheel Motors.", " ");
-                telemetry.update();
-                current_power = Range.clip(current_power, -1, 1);
-                Flywheel1.setPower(0.00);
-                Flywheel2.setPower(0.00);
-            }
+                // The code below is used to put the Flywheel Motor(s) at 100% Throttle IN REVERSE when the 'A' button is pushed on the Controller.
+                if (buttonA) {
+                    telemetry.addData("Flywheel Motors Engaged in Reverse. ", " ");
+                    telemetry.update();
+                    Flywheel1.setPower(-1.00);
+                    Flywheel2.setPower(-1.00);
+                }
+
+                if (buttonY) {
+                    telemetry.addData("All Flywheel Motors Engaged.", " ");
+                    telemetry.update();
+                    current_power = Range.clip(current_power, -1, 1);
+                    Flywheel1.setPower(1.00);
+                    Flywheel2.setPower(1.00);
+                }
+
+                if (LeftBumper) {
+                    loadingservo.setPosition(0.00);
+                    telemetry.addData("Closing.", " ");
+                    telemetry.update();
+                }
 
 
-            // The code below is used to put the Flywheel Motor(s) at 100% Throttle IN REVERSE when the 'A' button is pushed on the Controller.
-            if (buttonA) {
-                telemetry.addData("Flywheel Motors Engaged in Reverse. ", " ");
-                telemetry.update();
-                Flywheel1.setPower(-1.00);
-                Flywheel2.setPower(-1.00);
-            }
+                if (RightBumper) {
+                    loadingservo.setPosition(1.00);
+                    telemetry.addData("Opening.", " ");
+                    telemetry.update();
+                }
 
-            if (buttonY) {
-                telemetry.addData("All Flywheel Motors Engaged.", " ");
-                telemetry.update();
-                current_power = Range.clip(current_power, -1, 1);
-                Flywheel1.setPower(1.00);
-                Flywheel2.setPower(1.00);
-            }
+                if (dpad_up) {
 
-            if (LeftBumper) {
-                loadingservo.setPosition(0.00);
-                telemetry.addData("Closing.", " ");
-                telemetry.update();
-            }
+                    telemetry.addData("Motors set to Top Goal.", " ,");
+                    telemetry.update();
 
+                    Flywheel1.setPower(1.00);
+                    Flywheel2.setPower(1.00);
+                }
 
-            if (RightBumper) {
-                loadingservo.setPosition(1.00);
-                telemetry.addData("Opening.", " ");
-                telemetry.update();
-            }
+                if (dpad_right) {
 
-            if (dpad_up) {
+                    telemetry.addData("Motors set to Middle Goal.", " ,");
+                    telemetry.update();
 
-                telemetry.addData("Motors set to Top Goal.", " ,");
-                telemetry.update();
-
-                Flywheel1.setPower(1.00);
-                Flywheel2.setPower(1.00);
-            }
-
-            if (dpad_right) {
-
-                telemetry.addData("Motors set to Middle Goal.", " ,");
-                telemetry.update();
-
-                Flywheel1.setPower(0.60);
-                Flywheel2.setPower(0.60);
-            }
+                    Flywheel1.setPower(0.60);
+                    Flywheel2.setPower(0.60);
+                }
 
 
-            if (dpad_down) {
+                if (dpad_down) {
 
-                telemetry.addData("Motors set to Bottom Goal.", " ,");
-                telemetry.update();
+                    telemetry.addData("Motors set to Bottom Goal.", " ,");
+                    telemetry.update();
 
-                Flywheel1.setPower(0.30);
-                Flywheel2.setPower(0.30);
-            }
-
-
-            if (buttonX && GOOD) {
-                GOOD = false;
-                telemetry.addData("Current Power Level Is: ", current_power);
-
-                current_power = (current_power + 0.05);
-
-                telemetry.addData("Calculated Power", current_power);
-
-                current_power = Range.clip(current_power, -1, 1);
-
-                telemetry.addData("Clipped Power", current_power);
-
-                telemetry.addData("Motor 2 Power", Flywheel2.getPower());
-                telemetry.update();
-
-                Flywheel1.setPower(current_power);
-                Flywheel2.setPower(current_power);
-                GOOD = true;
-                sleep(200);
-            }
+                    Flywheel1.setPower(0.30);
+                    Flywheel2.setPower(0.30);
+                }
 
 
-            if (buttonA && GOOD) {
-                GOOD = false;
-                telemetry.addData("Current Power Level Is: ", current_power);
+                if (buttonX && GOOD) {
+                    GOOD = false;
+                    telemetry.addData("Current Power Level Is: ", current_power);
 
-                current_power = (current_power - 0.05);
+                    current_power = (current_power + 0.05);
 
-                telemetry.addData("Calculated Power", current_power);
+                    telemetry.addData("Calculated Power", current_power);
 
-                current_power = Range.clip(current_power, -1, 1);
+                    current_power = Range.clip(current_power, -1, 1);
 
-                telemetry.addData("clipped power", current_power);
-                telemetry.update();
+                    telemetry.addData("Clipped Power", current_power);
 
-                Flywheel1.setPower(current_power);
-                Flywheel2.setPower(current_power);
-                GOOD = true;
-                sleep(200);
-            }
-            
-            if (button2A == true){
-                intake.setPower(1.00);
-                
-            }
-            if (button2A == false);
-            intake.setPower(0.00);
+                    telemetry.addData("Motor 2 Power", Flywheel2.getPower());
+                    telemetry.update();
+
+                    Flywheel1.setPower(current_power);
+                    Flywheel2.setPower(current_power);
+                    GOOD = true;
+                    sleep(200);
+                }
+
+
+                if (buttonA && GOOD) {
+                    GOOD = false;
+                    telemetry.addData("Current Power Level Is: ", current_power);
+
+                    current_power = (current_power - 0.05);
+
+                    telemetry.addData("Calculated Power", current_power);
+
+                    current_power = Range.clip(current_power, -1, 1);
+
+                    telemetry.addData("clipped power", current_power);
+                    telemetry.update();
+
+                    Flywheel1.setPower(current_power);
+                    Flywheel2.setPower(current_power);
+                    GOOD = true;
+                    sleep(200);
+                }
 
             }
         }
@@ -253,4 +234,3 @@ public class teleop_program extends LinearOpMode {
 
 //     telemetry.addData("Status", "Ready to run");    //
 //              telemetry.update();
-
